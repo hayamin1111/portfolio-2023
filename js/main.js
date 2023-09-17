fa31146a810c267b21fb9831959c51440eaa785e
Emi Hayakawa <52098983+hayamin1111@users.noreply.github.com>
2023-09-18 08:06:46 +0900

    add scroll animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 // const buttons = document.querySelectorAll('button');
 
 // buttons.forEach((btn) => {
@@ -16,37 +20,77 @@
 // });
 
 //scroll連動アニメーション
-const targetElems = document.querySelectorAll('.section');
-
-const options = {
-  root: null,
-  rootMargin: "-20% 0px -20% 0px",
-  threshold: 0
-}
-
-const callback = (entries) => {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) {
-      changeColor(entry.target);
-      console.log('見えています。');
-    } else {
-      console.log('見えていません。');
-    }
-  });
-}
-
-const observer = new IntersectionObserver(callback, options);
-
-targetElems.forEach(targetElem => {
-  observer.observe(targetElem);
-});
-
-function changeColor(target){
-
-  const currentActiveSection = document.querySelector('.is-changeColor');
-  if(currentActiveSection !== null) {
-    console.log(currentActiveSection);
-    currentActiveSection.classList.remove('is-changeColor');
+scrollChangeColorFunc();
+function scrollChangeColorFunc() {
+  const targetElems = document.querySelectorAll('.section');
+  
+  const options = {
+    root: null,
+    rootMargin: "-20% 0px -20% 0px",
+    threshold: 0
   }
-  target.classList.add('is-changeColor');
-};
+  
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        changeColor(entry.target);
+        // console.log('見えています。');
+      } else {
+        // console.log('見えていません。');
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(callback, options);
+  
+  targetElems.forEach(targetElem => {
+    observer.observe(targetElem);
+  });
+  
+  function changeColor(target){
+  
+    const currentActiveSection = document.querySelector('.is-changeColor');
+    if(currentActiveSection !== null) {
+      currentActiveSection.classList.remove('is-changeColor');
+    }
+    target.classList.add('is-changeColor');
+  };
+}
+
+scrollChangeScaleFunc();
+function scrollChangeScaleFunc() {
+  const targetElems = document.querySelectorAll('.bg-svg');
+  
+  const options = {
+    root: null,
+    rootMargin: "0px 0px -70% 0px",
+    threshold: 0
+  }
+  
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        changeScale(entry.target);
+        console.log('見えています。');
+      } else {
+        console.log('見えていません。');
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(callback, options);
+  
+  targetElems.forEach(targetElem => {
+    observer.observe(targetElem);
+  });
+  
+  function changeScale(target){
+    target.classList.add('is_scale');
+    const currentActiveSvg = document.querySelector('.is_scale');
+    if(currentActiveSvg !== null) {
+      console.log(currentActiveSvg);
+      currentActiveSvg.classList.remove('is_scale');
+    }
+    target.classList.add('is_scale');
+  };
+}
